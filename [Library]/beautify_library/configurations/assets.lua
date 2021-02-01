afaff4525a7de3116b2d4fab9bbcd3ff1c8e15f1
Tron <crazynuggets@outlook.com>
2021-02-01 17:04:02 +0300

    Added asset's confign (In Dev)

---
+++ [Library]/beautify_library/configurations/assets.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------------
+--[[ Resource: Beautify Library
+     Script: configurations: assets.lua
+     Server: -
+     Author: OvileAmriam
+     Developer: -
+     DOC: 01/02/2021 (OvileAmriam)
+     Desc: Asset's Confign ]]--
+----------------------------------------------------------------
+
+
+------------------------
+--[[ Configurations ]]--
+------------------------
+
+availableAssets = {
+
+    ["images"] = {
+        --TODO: ADD ALL ASSET LISTS HERE
+    }
+
+}
