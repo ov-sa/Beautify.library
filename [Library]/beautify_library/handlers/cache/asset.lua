ab6154613628f2d1807877136e406ac646305827
Tron <crazynuggets@outlook.com>
2021-02-01 17:01:41 +0300

    Added asset's cache

---
+++ [Library]/beautify_library/handlers/cache/asset.lua
@@ -0,0 +1,31 @@
+----------------------------------------------------------------
+--[[ Resource: Beautify Library
+     Script: handlers: cache: asset.lua
+     Server: -
+     Author: OvileAmriam
+     Developer: -
+     DOC: 01/02/2021 (OvileAmriam)
+     Desc: Asset's Cache Handler ]]--
+----------------------------------------------------------------
+
+
+-------------------
+--[[ Variables ]]--
+-------------------
+
+createdAssets = {
+
+    ["images"] = {}
+
+}
+
+
+-----------------------------------------
+--[[ Event: On Client Resource Start ]]--
+-----------------------------------------
+
+addEventHandler("onClientResourceStart", resource, function()
+
+    --TODO: CREATE ASSETS
+
+end)
