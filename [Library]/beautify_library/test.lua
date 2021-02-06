a2e7c22fc044634b1928aff9290645f0802429d6
Tron <crazynuggets@outlook.com>
2021-02-06 23:48:03 +0300

    Update test.lua

--- [Library]/beautify_library/test.lua
+++ [Library]/beautify_library/test.lua
@@ -8,6 +8,6 @@ sX = sX/1366; sY = sY/768;
 addEventHandler("onClientResourceStart", resource, function()
 
     local window_width, window_height = 600, 350
-    createWindow((sX*1366 - window_width)/2, (sY*768 - window_height)/2, window_width, window_height, "My First Window", tocolor(15, 15, 15, 255), false)
+    createWindow((sX*1366 - window_width)/2, (sY*768 - window_height)/2, window_width, window_height, "MY FIRST WINDOW", tocolor(15, 15, 15, 255), false)
 
 end)
