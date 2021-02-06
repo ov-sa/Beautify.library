8a80ab5e10debc7689094ae80646445c9913c461
Tron <crazynuggets@outlook.com>
2021-02-06 13:34:23 +0300

    Removed old todo

--- [Library]/beautify_library/test.lua
+++ [Library]/beautify_library/test.lua
@@ -7,7 +7,6 @@ sX = sX/1366; sY = sY/768;
 
 addEventHandler("onClientResourceStart", resource, function()
 
-    --TODO: Throw error if negative values are entered, make config for it..
     local window_width, window_height = 600, 350
     createWindow((sX*1366 - window_width)/2, (sY*768 - window_height)/2, window_width, window_height, "My First Window", tocolor(5, 5, 5, 255))
 
