da3cabc989b6142795339468afefeec5a3bd8ae1
Tron <crazynuggets@outlook.com>
2021-02-07 21:56:27 +0300

    Update window.lua

--- [Library]/beautify_library/handlers/renderer/window.lua
+++ [Library]/beautify_library/handlers/renderer/window.lua
@@ -32,19 +32,29 @@ function renderWindow(element)
     dxDrawImage(window_startX, window_startY + window_height - window_borderSize, window_borderSize, window_borderSize, createdAssets["images"]["curved_square/bottom_left.png"], 0, 0, 0, window_color, window_postGUI)
     dxDrawImage(window_startX + window_width - window_borderSize, window_startY + window_height - window_borderSize, window_borderSize, window_borderSize, createdAssets["images"]["curved_square/bottom_right.png"], 0, 0, 0, window_color, window_postGUI)
     if window_width > availableElements["beautify_window"].__minimumSize then
-        dxDrawRectangle(window_startX + window_borderSize, window_startY, window_width - availableElements["beautify_window"].__minimumSize, window_borderSize, window_color, window_postGUI)
-        dxDrawRectangle(window_startX + window_borderSize, window_startY + window_height - window_borderSize, window_width - availableElements["beautify_window"].__minimumSize, window_borderSize, window_color, window_postGUI)
+        dxDrawRectangle(window_startX + window_borderSize, window_startY, window_width - availableElements["beautify_window"].__minimumSize, window_borderSize, window_color, window_postGUI, true)
+        dxDrawRectangle(window_startX + window_borderSize, window_startY + window_height - window_borderSize, window_width - availableElements["beautify_window"].__minimumSize, window_borderSize, window_color, window_postGUI, true)
     end
     if window_height > availableElements["beautify_window"].__minimumSize then
-        dxDrawRectangle(window_startX, window_startY + window_borderSize, window_borderSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI)
-        dxDrawRectangle(window_startX + window_width - window_borderSize, window_startY + window_borderSize, window_borderSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI)
+        dxDrawRectangle(window_startX, window_startY + window_borderSize, window_borderSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI, true)
+        dxDrawRectangle(window_startX + window_width - window_borderSize, window_startY + window_borderSize, window_borderSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI, true)
     end
     if window_width > availableElements["beautify_window"].__minimumSize and window_height > availableElements["beautify_window"].__minimumSize then
-        dxDrawRectangle(window_startX + window_borderSize, window_startY + window_borderSize, window_width - availableElements["beautify_window"].__minimumSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI)
+        dxDrawRectangle(window_startX + window_borderSize, window_startY + window_borderSize, window_width - availableElements["beautify_window"].__minimumSize, window_height - availableElements["beautify_window"].__minimumSize, window_color, window_postGUI, true)
     end
     if (window_width >= window_titleBar_height) and (window_height >= window_titleBar_height) then
+        local isCloseButtonHovered = false
         local window_close_button_startX, window_close_button_startY = window_startX + window_width - window_titleBar_height, window_startY
-        local isCloseButtonHovered = isMouseOnPosition(window_close_button_startX, window_close_button_startY, window_titleBar_height, window_titleBar_height)
+        local isTitleBarClicked = isMouseOnPosition(window_startX, window_startY, window_width - window_titleBar_height, window_titleBar_height) and isLMBClicked
+        if isTitleBarClicked then
+            Timer(function(element)
+                attachElement(element)
+            end, 1, 1, element)
+        else
+            if not getAttachedElement() then
+                isCloseButtonHovered = isMouseOnPosition(window_close_button_startX, window_close_button_startY, window_titleBar_height, window_titleBar_height)
+            end
+        end
         if isCloseButtonHovered then
             if isLMBClicked then
                 Timer(function(element)
@@ -68,7 +78,7 @@ function renderWindow(element)
             createdElements[element].gui.titleBar.close_button.animAlphaPercent = interpolateBetween(createdElements[element].gui.titleBar.close_button.animAlphaPercent, 0, 0, availableElements["beautify_window"].__titleBar.close_button.minmumAnimPercent, 0, 0, getInterpolationProgress(createdElements[element].gui.titleBar.close_button.hoverAnimTickCounter, availableElements["beautify_window"].__titleBar.close_button.hoverAnimDuration), "InQuad")
         end
         dxDrawImage(window_startX, window_startY, window_titleBar_height, window_titleBar_height, createdAssets["images"]["curved_square/top_left.png"], 0, 0, 0, tocolor(unpack(window_titleBar_color)), window_postGUI)
-        dxDrawRectangle(window_startX + window_titleBar_height, window_startY, window_width - (window_titleBar_height*2), window_titleBar_height, tocolor(unpack(window_titleBar_color)), window_postGUI)
+        dxDrawRectangle(window_startX + window_titleBar_height, window_startY, window_width - (window_titleBar_height*2), window_titleBar_height, tocolor(unpack(window_titleBar_color)), window_postGUI, true)
         dxDrawImage(window_close_button_startX, window_close_button_startY, window_titleBar_height, window_titleBar_height, createdAssets["images"]["curved_square/top_right.png"], 0, 0, 0, tocolor(window_titleBar_color[1], window_titleBar_color[2], window_titleBar_color[3], window_titleBar_color[4]*createdElements[element].gui.titleBar.close_button.animAlphaPercent), window_postGUI)
         dxDrawText("X", window_close_button_startX, window_close_button_startY, window_close_button_startX + window_titleBar_height, window_startY + window_titleBar_height, tocolor(createdElements[element].gui.titleBar.close_button.fontColor[1], createdElements[element].gui.titleBar.close_button.fontColor[2], createdElements[element].gui.titleBar.close_button.fontColor[3], createdElements[element].gui.titleBar.close_button.fontColor[4]*createdElements[element].gui.titleBar.close_button.animAlphaPercent), 1, createdElements[element].gui.titleBar.font, "center", "center", true, false, window_postGUI, false, true)
         dxDrawText(createdElements[element].gui.title, window_startX + window_titleBar_paddingX, window_startY, window_startX + window_width - window_titleBar_height - window_titleBar_paddingX, window_startY + window_titleBar_height, tocolor(unpack(createdElements[element].gui.titleBar.fontColor)), 1, createdElements[element].gui.titleBar.font, "center", "center", true, false, window_postGUI, false, true)
@@ -86,9 +96,6 @@ function renderWindow(element)
         dxSetBlendMode("add")
         dxDrawImage(window_renderTarget_startX, window_renderTarget_startY, window_renderTarget_width, window_renderTarget_height, window_renderTarget, 0, 0, 0, tocolor(255, 255, 255, 255), window_postGUI)
         dxSetBlendMode("blend")
-    end
-    if isLMBClicked then
-        attachElement(element)
     end
     return true
 
