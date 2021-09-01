1b1e4a88ca32e7b13117c652445a8e22b83b710a
Aviril <avirilaviril@protonmail.com>
2021-09-01 14:51:21 +0300

    Update deck.lua

--- [Library]/beautify_library/configurations/templates/deck.lua
+++ [Library]/beautify_library/configurations/templates/deck.lua
@@ -14,7 +14,7 @@
 ------------------------
 
 availableTemplates["beautify_deck"] = {
-    color = {0, 0, 0, 253},
+    color = {5, 5, 5, 253},
     titleBar = {
         isOutLine = true,
         font = dxCreateFont("files/assets/fonts/signika_semibold.ttf", 11),
