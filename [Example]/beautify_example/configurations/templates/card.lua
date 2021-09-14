895d0c6bdc9170ce7de8a1cf98c8cd863df9c235
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-09-14 16:52:55 +0300

    Added card example template

---
+++ [Example]/beautify_example/configurations/templates/card.lua
@@ -0,0 +1,28 @@
+----------------------------------------------------------------
+--[[ Resource: Beautify Library (Example) 
+     Script: configurations: templates: window.lua
+     Server: -
+     Author: OvileAmriam
+     Developer: -
+     DOC: 14/08/2021 (OvileAmriam)
+     Desc: Template's Confign ]]--
+----------------------------------------------------------------
+
+
+------------------------
+--[[ Configurations ]]--
+------------------------
+
+availableTemplates["beautify_card"] = {
+
+    ["dark-silver-theme"] = {},
+
+    ["dark-blue-theme"] = {},
+
+    ["dark-red-theme"] = {},
+
+    ["dark-green-theme"] = {},
+
+    ["dark-yellow-theme"] = {}
+
+}
