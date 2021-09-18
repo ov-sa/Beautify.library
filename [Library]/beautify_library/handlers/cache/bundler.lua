b511c479ff9a3d8fd5519aea1186681e4765c6cc
Aviril <89867985+Aviril@users.noreply.github.com>
2021-09-18 18:41:46 +0300

    Update bundler.lua

--- [Library]/beautify_library/handlers/cache/bundler.lua
+++ [Library]/beautify_library/handlers/cache/bundler.lua
@@ -18,6 +18,9 @@ local imports = {
     ipairs = ipairs,
     getResourceName = getResourceName,
     addEventHandler = addEventHandler,
+    table = {
+        insert = table.insert
+    },
     string = {
         sub = string.sub,
         gsub = string.gsub,
@@ -37,11 +40,19 @@ local bundlerData = false
 --[[ Function: Fetches Imports ]]--
 -----------------------------------
 
-function fetchImports()
+function fetchImports(recieveData)
 
     if not bundlerData then return false end
 
-    return bundlerData
+    if recieveData == true then
+        return bundlerData
+    else
+        return [[
+        for i, j in ipairs(exports.beautify_library:fetchImports(true)) do
+            loadstring(j)()
+        end
+        ]]
+    end
 
 end
 
@@ -58,7 +69,8 @@ imports.addEventHandler("onClientResourceStart", resource, function(resourceSour
         utilities = fetchFileData("utilities/client.lua"),
         settings = fetchFileData("settings/client.lua"),
         handlers = {
-            input = fetchFileData("handlers/element/input.lua")
+            input = fetchFileData("handlers/element/input.lua"),
+            bundler = ""
         }
     }
 
@@ -66,17 +78,28 @@ imports.addEventHandler("onClientResourceStart", resource, function(resourceSour
     cloneUIOutline = nil
     areUIParametersValid = nil
     ]]
+
     imports.string.gsub(importedFiles.utilities, "if CLIENT_ATTACHED_ELEMENT then return false end", "")
     importedFiles.settings = importedFiles.settings..[[
     UI_VALID_ALIGNMENT = nil
     UI_VALID_SCROLLERS = nil
     ]]
 
-    bundlerData = [[
-    ]]..importedFiles.utilities..[[
-    ]]..importedFiles.settings..[[
-    ]]..importedFiles.handlers.input..[[
-    local BEAUTIFY_LIBRARY = {
+    importedFiles.handlers.input = importedFiles.handlers.input..[[
+    CLIENT_HOVERED_ELEMENT = nil
+    local imports = {
+        addEventHandler = addEventHandler,
+        resetKeyClickCache = resetKeyClickCache,
+        resetScrollCache = resetScrollCache
+    }
+    imports.addEventHandler("onClientRender", root, function()
+        CLIENT_MTA_RESTORED = false
+        imports.resetKeyClickCache()
+        imports.resetScrollCache()
+    end, false, UI_PRIORITY_LEVEL.RENDER)]]
+
+    importedFiles.handlers.bundler = [[
+    local imports = {
         call = call,
         type = type,
         addEventHandler = addEventHandler,
@@ -85,11 +108,20 @@ imports.addEventHandler("onClientResourceStart", resource, function(resourceSour
         renderClass = {},
         functionClass = {}
     }
-    BEAUTIFY_LIBRARY.renderClass.__index = BEAUTIFY_LIBRARY.renderClass
-    function BEAUTIFY_LIBRARY.renderClass:createRender(functionReference, elementReference)
-        if not functionReference or (BEAUTIFY_LIBRARY.type(functionReference) ~= "function") then return false end
+    imports.renderClass.__index = imports.renderClass
+    function imports.renderClass:createRender(functionReference, elementReference, ...)
+        if not functionReference or (imports.type(functionReference) ~= "function") then return false end
         if not elementReference then
-            BEAUTIFY_LIBRARY.addEventHandler("onClientRender", root, functionReference, false, UI_PRIORITY_LEVEL.RENDER)
+            if beautify.render.NON_ELEMENT_RENDERS[functionReference] then return false end
+            self = setmetatable({}, self) 
+            self.functionReference = functionReference
+            self.elementReference = false
+            self.cbArguments = {...}
+            self.renderFunction = function()
+                self.functionReference(self.cbArguments)
+            end
+            beautify.render.NON_ELEMENT_RENDERS[functionReference] = self
+            imports.addEventHandler("onClientRender", root, self.renderFunction, false, UI_PRIORITY_LEVEL.RENDER)
         else
             if not beautify.isUIValid(elementReference) then return false end
             if not beautify.render.ELEMENT_RENDERS[elementReference] then
@@ -102,28 +134,24 @@ imports.addEventHandler("onClientResourceStart", resource, function(resourceSour
             self = setmetatable({}, self) 
             self.functionReference = functionReference
             self.elementReference = elementReference
+            self.cbArguments = {...}
             self.renderFunction = function()
-                self.functionReference(self.elementReference, beautify.getUIPosition(self.elementReference))
+                self.functionReference(self.elementReference, beautify.getUIPosition(self.elementReference), self.cbArguments)
             end
             beautify.render.ELEMENT_RENDERS[elementReference].renderFunctions[functionReference] = self
             beautify.render.ELEMENT_RENDERS[elementReference].totalFunctions = beautify.render.ELEMENT_RENDERS[elementReference].totalFunctions + 1
         end
         return true 
     end
-    BEAUTIFY_LIBRARY.addEventHandler("onClientUIPostViewRTRender", root, function()
-        if beautify.render.ELEMENT_RENDERS[source] then
-            for i, j in pairs(beautify.render.ELEMENT_RENDERS[source].renderFunctions) do
-                j.renderFunction()
-            end
-        end
-    end)
-    function BEAUTIFY_LIBRARY.renderClass:removeRender(functionReference, elementReference)
-        if not functionReference or (BEAUTIFY_LIBRARY.type(functionReference) ~= "function") then return false end
+    function imports.renderClass:removeRender(functionReference, elementReference)
+        if not functionReference or (imports.type(functionReference) ~= "function") then return false end
         if not elementReference then
-            BEAUTIFY_LIBRARY.removeEventHandler("onClientRender", root, functionReference)
+            if not beautify.render.NON_ELEMENT_RENDERS[functionReference] then return false end
+            imports.removeEventHandler("onClientRender", root, beautify.render.NON_ELEMENT_RENDERS[functionReference].renderFunction)
+            beautify.render.NON_ELEMENT_RENDERS[functionReference] = nil
         else
             if not beautify.render.ELEMENT_RENDERS[elementReference] or not beautify.render.ELEMENT_RENDERS[elementReference].renderFunctions[functionReference] then return false end
-            BEAUTIFY_LIBRARY.removeEventHandler("onClientRender", root, beautify.render.ELEMENT_RENDERS[elementReference].renderFunctions[functionReference].renderFunction)
+            imports.removeEventHandler("onClientRender", root, beautify.render.ELEMENT_RENDERS[elementReference].renderFunctions[functionReference].renderFunction)
             beautify.render.ELEMENT_RENDERS[elementReference].renderFunctions[functionReference] = nil
             beautify.render.ELEMENT_RENDERS[elementReference].totalFunctions = beautify.render.ELEMENT_RENDERS[elementReference].totalFunctions - 1
             if beautify.render.ELEMENT_RENDERS[elementReference].totalFunctions <= 0 then
@@ -132,41 +160,53 @@ imports.addEventHandler("onClientResourceStart", resource, function(resourceSour
         end
         return true
     end
-    function BEAUTIFY_LIBRARY.functionClass:__index(functionName)
+    imports.addEventHandler("onClientUIPostViewRTRender", root, function()
+        if beautify.render.ELEMENT_RENDERS[source] then
+            for i, j in pairs(beautify.render.ELEMENT_RENDERS[source].renderFunctions) do
+                j.renderFunction()
+            end
+        end
+    end)
+    function imports.functionClass:__index(functionName)
         self[functionName] = function(...)
-            return BEAUTIFY_LIBRARY.call(BEAUTIFY_LIBRARY.resource, functionName, ...)
+            return imports.call(imports.resource, functionName, ...)
         end
         return self[functionName]
     end
-    BEAUTIFY_LIBRARY.functionInit = setmetatable({}, BEAUTIFY_LIBRARY.functionClass)
+    imports.functionInit = setmetatable({}, imports.functionClass)
     beautify = {
         render = {
-            ELEMENT_RENDERS = {},
+            NON_ELEMENT_RENDERS = {}, ELEMENT_RENDERS = {},
             create = function(...)
-                return BEAUTIFY_LIBRARY.renderClass:createRender(...)
+                return imports.renderClass:createRender(...)
             end,
             remove = function(...)
-                return BEAUTIFY_LIBRARY.renderClass:removeRender(...)
+                return imports.renderClass:removeRender(...)
             end
         }, ]]
-
     for i, j in imports.pairs(availableElements) do
         if i == "__ELEMENT_ESSENTIALS__" then
             for k, v in imports.ipairs(j) do
-                bundlerData = bundlerData..v..[[ = BEAUTIFY_LIBRARY.functionInit.]]..v..[[,]]
+                importedFiles.handlers.bundler = importedFiles.handlers.bundler..v..[[ = imports.functionInit.]]..v..[[,]]
             end
         else
             if j.reference then
                 local elementName = imports.string.upper(imports.string.sub(j.reference, 1, 1))..imports.string.sub(j.reference, 2)
-                bundlerData = bundlerData..[[["]]..j.reference..[["] = {]]
-                bundlerData = bundlerData..(imports.string.gsub(j.syntax.functionName, elementName, "", 1))..[[ = BEAUTIFY_LIBRARY.functionInit.]]..j.syntax.functionName..[[,]]
+                importedFiles.handlers.bundler = importedFiles.handlers.bundler..[[["]]..j.reference..[["] = {]]
+                importedFiles.handlers.bundler = importedFiles.handlers.bundler..(imports.string.gsub(j.syntax.functionName, elementName, "", 1))..[[ = imports.functionInit.]]..j.syntax.functionName..[[,]]
                 for k, v in imports.pairs(j.APIs) do
-                    bundlerData = bundlerData..(imports.string.gsub(k, elementName, "", 1))..[[ = BEAUTIFY_LIBRARY.functionInit.]]..k..[[,]]
+                    importedFiles.handlers.bundler = importedFiles.handlers.bundler..(imports.string.gsub(k, elementName, "", 1))..[[ = imports.functionInit.]]..k..[[,]]
                 end
-                bundlerData = bundlerData..[[},]]
+                importedFiles.handlers.bundler = importedFiles.handlers.bundler..[[},]]
             end
         end
     end
-    bundlerData = bundlerData..[[}]]
+    importedFiles.handlers.bundler = importedFiles.handlers.bundler..[[}]]
+
+    bundlerData = {}
+    imports.table.insert(bundlerData, importedFiles.utilities)
+    imports.table.insert(bundlerData, importedFiles.settings)
+    imports.table.insert(bundlerData, importedFiles.handlers.input)
+    imports.table.insert(bundlerData, importedFiles.handlers.bundler)
 
 end)
